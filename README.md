7d559a54ca404877514f77a9cceb8afce4abedc6
Andreas Seeger <splash@appfros.ch>
2020-09-28 14:33:31 +0200

    Finished running version 1.0.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# SwiftlyAppleMailCreator
+
+## Purpose
+Command Line Tool / Executable to _compose_ a new mail.
+
+This executable is mainly to be used with Apple Mail, as attaching a file is for 
+reasons that are beyond the scope of this project does _not_ work with MS Outlook.
+
+## Versioning
+2020-09-28 Version 1.0.0
+
+## Required Arguments
+- Email Adress
+- Subject
+- Message Body
+- Absolut path to the attachment file
+
+## Security
+Sandboxing is deactivated for this small project.
+
+## Disclaimer
+As mentioned above, this project is meant to work with Apple Mail and is conformed working 
+with that Mail client on
+- macOS 10.15.7
+- (to be continued)
--- SwiftlyAppleMailCreator/main.swift
+++ SwiftlyAppleMailCreator/main.swift
@@ -6,6 +6,33 @@
 //
 
 import Foundation
+import AppKit
 
-print("Hello, World!")
+let app = NSApplication.shared
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        guard CommandLine.argc == 5 else { fatalError() }
+        let recipient = CommandLine.arguments[1]
+        let subject = CommandLine.arguments[2]
+        let body = CommandLine.arguments[3]
+        let attachmentPath = CommandLine.arguments[4]
+        SendEmail.send(recipient: recipient, subject: subject, body: body, attachmentPath: attachmentPath)
+    }
+}
+
+class SendEmail: NSObject {
+    static func send(recipient: String, subject: String, body: String, attachmentPath: String) {
+        guard let service = NSSharingService(named: .composeEmail) else { fatalError() }
+        service.recipients = [recipient]
+        service.subject = subject
+        
+        let attmtUrl = URL(fileURLWithPath: attachmentPath).absoluteURL
+        
+        service.perform(withItems: [body, attmtUrl as URL])
+    }
+}
+
+let delegate = AppDelegate()
+app.delegate = delegate
+app.run()
