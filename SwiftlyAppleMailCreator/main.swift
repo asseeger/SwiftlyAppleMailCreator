c2fd5d76ac9bcb4bda74fc7991299e26eaeedd39
Andreas Seeger <splash@appfros.ch>
2020-09-28 15:18:50 +0200

    asyncAfter to wait for execution

--- SwiftlyAppleMailCreator/main.swift
+++ SwiftlyAppleMailCreator/main.swift
@@ -12,18 +12,22 @@ let app = NSApplication.shared
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
-        guard CommandLine.argc == 5 else { fatalError() }
+        guard CommandLine.argc == 5 else { exit(-1) }
         let recipient = CommandLine.arguments[1]
         let subject = CommandLine.arguments[2]
         let body = CommandLine.arguments[3]
         let attachmentPath = CommandLine.arguments[4]
         SendEmail.send(recipient: recipient, subject: subject, body: body, attachmentPath: attachmentPath)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    exit(0)
+                }
     }
 }
 
 class SendEmail: NSObject {
     static func send(recipient: String, subject: String, body: String, attachmentPath: String) {
-        guard let service = NSSharingService(named: .composeEmail) else { fatalError() }
+        guard let service = NSSharingService(named: .composeEmail) else { exit(-1) }
         service.recipients = [recipient]
         service.subject = subject
         
